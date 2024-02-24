612811ac02297955934e0b4e504da5905f7cb0fb
Brian Cordoba <briancormin@gmail.com>
2024-02-24 10:08:50 -0500

    Mongo URI changed

--- routers/routers.js
+++ routers/routers.js
@@ -4,7 +4,7 @@ import usersController from "../controllers/controllers.js";
 const mainRouter = Router();
 
 mainRouter.post('/', usersController.createNewuser);
-mainRouter.get('/', usersController.seeAllUsers);
+mainRouter.get('https://gymusersdata-405b6a41e1bf.herokuapp.com', usersController.seeAllUsers);
 mainRouter.get('/:id', usersController.seeOneUser);
 mainRouter.put('/:id', usersController.updateUserInfo);
 mainRouter.delete('/:id', usersController.deleteUser);
